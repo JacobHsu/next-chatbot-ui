32473fe2e7f16de982dbc5d3f4945b6f92d11e14
jacobhsu <jacobhsu@jacobhsuMacBook-Pro.local>
2024-10-23 17:56:20 +0800

    feat: @radix-ui/react-tabs

--- NOTE.md
+++ NOTE.md
@@ -1,4 +1,10 @@
 # Note
 
+紅線底 路徑抓不到 剪下重貼  
+
 `clsx` 是一個輕量的 npm 套件，用來條件式地合併 CSS class 名稱  
 `tailwind-merge` 是一個用來處理 Tailwind CSS 類別合併  
+
+
+[@radix-ui/react-tabs](https://www.radix-ui.com/primitives/docs/components/tabs)  
+[@radix-ui/react-tooltip](https://www.radix-ui.com/primitives/docs/components/tooltip)  
--- components/ui/dashboard.tsx
+++ components/ui/dashboard.tsx
@@ -1,12 +1,13 @@
 "use client"
 
+import { SidebarSwitcher } from "@/components/sidebar/sidebar-switcher"
 // import { Button } from "@/components/ui/button"
-// import { Tabs } from "@/components/ui/tabs"
+import { Tabs } from "@/components/ui/tabs"
 // import useHotkey from "@/lib/hooks/use-hotkey"
 import { cn } from "@/lib/utils" // 可能代表 class names
 import { ContentType } from "@/types"
 // import { IconChevronCompactRight } from "@tabler/icons-react"
-// import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { FC, useState } from "react"
 
 // import { CommandK } from "../utility/command-k"
@@ -19,7 +20,14 @@ interface DashboardProps {
 
 export const Dashboard: FC<DashboardProps> = ({ children }) => {
   // useHotkey("s", () => setShowSidebar(prevState => !prevState))
+  const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const tabValue = searchParams.get("tab") || "chats"
 
+  const [contentType, setContentType] = useState<ContentType>(
+    tabValue as ContentType
+  )
 
   const [showSidebar, setShowSidebar] = useState(
     localStorage.getItem("showSidebar") === "true"
@@ -48,7 +56,20 @@ export const Dashboard: FC<DashboardProps> = ({ children }) => {
           width: showSidebar ? `${SIDEBAR_WIDTH}px` : "0px"
         }}
       >
+          {showSidebar && (
+            <Tabs
+              className="flex h-full"
+              value={contentType}
+              onValueChange={tabValue => {
+                setContentType(tabValue as ContentType)
+                router.replace(`${pathname}?tab=${tabValue}`)
+              }}
+            >
+              <SidebarSwitcher onContentTypeChange={setContentType} />
 
+              {/* <Sidebar contentType={contentType} showSidebar={showSidebar} /> */}
+            </Tabs>
+          )}
       </div>
     </div>
   )
